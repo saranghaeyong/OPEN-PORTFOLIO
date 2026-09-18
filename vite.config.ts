1ad2476464f56a1d4c28e931bb04d8a15e44cae1
saranghaeyong <rnsarang@gmail.com>
2026-09-18 11:20:43 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/newwwwww/',
+    base: '/OPEN-PORTFOLIO/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
